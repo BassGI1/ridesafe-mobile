c66fd9eebcdb2c3fc5f52285d2313b2e2dde9e39
Bassam <basmaym148@gmail.com>
2025-05-31 17:18:18 -0400

    parallelized the model calling

--- server.py
+++ server.py
@@ -6,21 +6,25 @@ from socketio import AsyncServer
 from ultralytics.utils import LOGGER
 from aiohttp.web import Application, run_app
 from cv2 import VideoCapture, imencode, resize, rectangle, putText, FONT_ITALIC
+from time import sleep
+from threading import Thread
 
 app = Application()
 server = AsyncServer(cors_allowed_origins="*")
 server.attach(app)
 cam = VideoCapture(0)
 LOGGER.setLevel(CRITICAL)
+current_connections = set()
 
 
 @server.event
 async def connect(socket_id, env):
     print(f"socket with id: {socket_id} connected")
+    current_connections.add(socket_id)
 
 
 def object_outlines(image, model, labels):
-    results = model(image, conf=0.5)
+    results = model(image, conf=0.3)
     res = []
     for result in results:
         for box in result.boxes:
@@ -33,38 +37,43 @@ def object_outlines(image, model, labels):
     return res
 
 
-async def stream_video():
+def outline_updater(socket_id, outline, frame, model, labels):
+    while socket_id in current_connections:
+        outline[0] = object_outlines(frame[0], model, labels)
+        sleep(0.5)
+
+
+async def stream_video(socket_id):
     model = YOLO('yolov8n.pt')
-    labels = {"car", "truck", "bus", "person"}
+    labels = {"person", "car", "bus", "truck"}
 
-    outlines = []
-    frame_count = 0
+    outlines = [[]]
+    frame = [None]
+    model_thread = Thread(target=outline_updater, args=(
+        socket_id, outlines, frame, model, labels))
+    model_thread.start()
 
-    while 1:
-        success, frame = cam.read()
-        frame = resize(frame, (300, 200))
-        frame_count = (frame_count + 1) % 20
+    while socket_id in current_connections:
+        success, f = cam.read()
+        frame[0] = resize(f, (300, 200))
 
-        if frame_count == 19:
-            outlines = object_outlines(frame, model, labels)
-
-        for x1, y1, x2, y2, label in outlines:
-            rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            putText(frame, label, (x1, y1 - 10), FONT_ITALIC,
-                    0.5, (0, 255, 0), 2)
+        for x1, y1, x2, y2, label in outlines[0]:
+            rectangle(frame[0], (x1, y1), (x2, y2), (0, 255, 0), 1)
+            putText(frame[0], label, (x1, y1 - 10),
+                    FONT_ITALIC, 0.5, (0, 255, 0), 1)
 
         if not success:
             continue
-        _, buffer = imencode('.jpg', frame)
+        _, buffer = imencode('.jpg', frame[0])
 
         jpg_as_text = b64encode(buffer).decode('utf-8')
         await server.emit('video_frame', jpg_as_text)
 
 
 @server.event
-async def start_stream(sid):
+async def start_stream(socket_id):
     print("Stream requested")
-    create_task(stream_video())
+    create_task(stream_video(socket_id))
 
 
 run_app(app, host="0.0.0.0", port=8000)
